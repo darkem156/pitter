850a7781abdd958fc2e0f9ac92a8b923487da90e
Darkem <emmanuelchan809@gmail.com>
2022-09-25 13:24:09 -0500

    Responsive design

--- src/app/components/App.vue
+++ src/app/components/App.vue
@@ -1,79 +1,71 @@
 <template>
-    <div>
-        <div v-show="data.error.state" class="alert alert-danger alert-dismissible fade show" role="alert" id="alert">
-            {{data.error.error}}
-            <button v-on:click="closeAlert" type="button" class="close" id="close-alert">
-            <span>&times;</span>
-            </button>
-        </div>
-        <div>
-            <div v-if="!data.sesion" id="sign-container">
-                <div v-if="data.logIn">
-                    <form @submit.prevent="logIn" id="form-sign-in">
-                        <div>
-                            <input type="text" placeholder="Email o Usuario" class="text-sign" v-model="datos.dato">
-                        </div>
-                        <div>
-                            <input type="password" placeholder="Password" class="text-sign" v-model="datos.password">
-                        </div>
-                        <button class="button-sign">Sign In</button>
-                    </form>
-                    <button v-on:click="changeForm" class="changeForm" >No tengo una cuenta</button>
-                </div>
-                <div v-else>
-                    <form @submit.prevent="signUp">
-                        <div>
-                            <input class="text-sign" placeholder="name" v-model="datos.name" type="text">
-                        </div>
-                        <div>
-                            <input class="text-sign" placeholder="user name" v-model="datos.user_name" type="text" name="" id="">
-                        </div>
-                        <div>
-                            <input class="text-sign" placeholder="email" v-model="datos.email" type="text">
-                        </div>
-                        <div>
-                            <input class="text-sign" placeholder="password" v-model="datos.password" type="password" name="" id="">
-                        </div>
-                        <button class="button-sign">Sign Up</button>
-                    </form>
-                    <button v-on:click="changeForm" class="changeForm">Ya tengo una cuenta</button>
-                </div>
-            </div>
-            <div v-else>
-                <div>
-                    <div>
-                        <form @submit.prevent="publicar" id="form-publish">
-                            <div id="text-publish-container">
-                                <input type="text" placeholder="¡Publica algo!" v-model="data.content" name="" id="text-publish">
-                            </div>
-                            <div id="button-publish-container">
-                                <button id="button-publish">Publicar</button>
-                            </div>
-                        </form>
-                    </div>
-                    <div v-for="item in data.array" :key="item">
-                        <div class="publication-container">
-                            <div class="publication-data">
-                                <div class="user-data">
-                                    <div class="nombreUsuario"><a v-bind:href="'/user/' + item.id_usuario">{{item.name}}</a></div>
-                                    <div class="user-name"><p>@{{item.user_name}}</p></div>
-                                </div>  
-                                <div class="fecha">
-                                    <div></div>
-                                    <div></div>
-                                    <div class="fecha-fecha">{{item.date.split('.')[0].split('T')[0].replace('-', '/').replace('-', '/')}}</div>
-                                    <div class="fecha-hora"><p>{{item.date.split('.')[0].split('T')[1].substring(0, 5)}}</p></div>
-                                </div>
-                            </div>
-                            <div class="publication-content">
-                                <p>{{item.contenido}}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div>
+    <div v-show="data.error.state" class="alert alert-danger alert-dismissible fade show" role="alert" id="alert">
+      {{data.error.error}}
+      <button v-on:click="closeAlert" type="button" class="close" id="close-alert">
+        <span>&times;</span>
+      </button>
     </div>
+    <div v-if="!data.sesion" id="sign-container">
+      <div v-if="data.logIn">
+        <form @submit.prevent="logIn" id="form-sign-in">
+          <input type="text" placeholder="Email o Usuario" class="text-sign" v-model="datos.dato">
+          <input type="password" placeholder="Password" class="text-sign" v-model="datos.password">
+          <button class="button-sign">Sign In</button>
+        </form>
+        <button v-on:click="changeForm" class="changeForm" >No tengo una cuenta</button>
+      </div>
+      <div v-else>
+        <form @submit.prevent="signUp">
+          <div>
+            <input class="text-sign" placeholder="name" v-model="datos.name" type="text">
+          </div>
+          <div>
+            <input class="text-sign" placeholder="user name" v-model="datos.user_name" type="text" name="" id="">
+          </div>
+          <div>
+            <input class="text-sign" placeholder="email" v-model="datos.email" type="text">
+          </div>
+          <div>
+            <input class="text-sign" placeholder="password" v-model="datos.password" type="password" name="" id="">
+          </div>
+          <button class="button-sign">Sign Up</button>
+        </form>
+        <button v-on:click="changeForm" class="changeForm">Ya tengo una cuenta</button>
+      </div>
+    </div>
+    <div v-else>
+      <div id="form-publish-container">
+        <form @submit.prevent="publicar" id="form-publish">
+          <div id="text-publish-container">
+            <input type="text" placeholder="¡Publica algo!" v-model="data.content" name="" id="text-publish">
+          </div>
+          <div id="button-publish-container">
+            <button id="button-publish">Publicar</button>
+          </div>
+        </form>
+      </div>
+      <div v-for="item in data.array" :key="item">
+        <div class="publication-container">
+          <div class="publication-data">
+            <div class="user-data">
+              <div class="nombreUsuario"><a v-bind:href="'/user/' + item.id_usuario">{{item.name}}</a></div>
+              <div class="user-name"><p>@{{item.user_name}}</p></div>
+            </div>  
+            <div class="fecha">
+            <div></div>
+            <div></div>
+            <div class="fecha-fecha">{{item.date.split('.')[0].split('T')[0].replace('-', '/').replace('-', '/')}}</div>
+            <div class="fecha-hora"><p>{{item.date.split('.')[0].split('T')[1].substring(0, 5)}}</p></div>
+            </div>
+          </div>
+          <div class="publication-content">
+            <p>{{item.contenido}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
